0dd77bee7f931fb04d58cc5fdd02060bd491594d
mesh3l-966 <mesh3l-966@hotmail.com>
2017-11-19 02:38:20 +0300

    Initial commit

---
+++ app/src/main/java/com/example/meshal/seniorprorject_interfaces/MainList.java
@@ -0,0 +1,13 @@
+package com.example.meshal.seniorprorject_interfaces;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_list);
+    }
+}
